93753ab1898f53802935cd9e7e0d229502d0958b
QuocDai1011 <146009504+QuocDai1011@users.noreply.github.com>
2025-11-15 00:48:10 +0700

    ...

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,89 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Fragment } from 'react';
+import { Toaster } from 'sonner';
+
 import { AuthProvider } from './context/authContext';
 import { AuthGuard } from './context/AuthGuard';
+
+import { DefaultLayout } from './layouts';
+import { publicRoutes } from './routes';
+
 import SignInForm from './pages/Login/signin-form';
 import SignUpForm from './pages/Logup/signup-form';
 import HomePage from './pages/Home/HomePage';
-import { Toaster } from 'sonner';
 import Admin from './pages/Admin';
 
 function App() {
     return (
         <>
             <Toaster richColors position="bottom-right" theme="light" />
-            <BrowserRouter>
-                <AuthProvider>
-                    <Routes>
-                        <Route path="/login" element={<SignInForm />} />
-                        <Route path="/register" element={<SignUpForm />} />
-                        <Route
-                            path="/admin"
-                            element={
-                                <AuthGuard roles={['Admin']}>
-                                    <Admin />
-                                </AuthGuard>
-                            }
-                        />
-                        <Route
-                            path="/tearcher"
-                            element={
-                                <AuthGuard roles={['Teacher']}>
-                                    <Admin />
-                                </AuthGuard>
-                            }
-                        />
-                        <Route
-                            path="/"
-                            element={
-                                <AuthGuard roles={['Student']}>
-                                    <HomePage />
-                                </AuthGuard>
-                            }
-                        />
-                    </Routes>
-                </AuthProvider>
-            </BrowserRouter>
+
+            <Router>
+                <div className="App">
+                    <AuthProvider>
+                        <Routes>
+                            {/* ============================
+                                PUBLIC ROUTES
+                            ============================ */}
+                            {publicRoutes.map((route, index) => {
+                                const Page = route.component;
+                                let Layout = DefaultLayout;
+
+                                if (route.layout) Layout = route.layout;
+                                else if (route.layout === null) Layout = Fragment;
+
+                                return (
+                                    <Route
+                                        key={index}
+                                        path={route.path}
+                                        element={
+                                            <Layout>
+                                                <Page />
+                                            </Layout>
+                                        }
+                                    />
+                                );
+                            })}
+
+                            {/* ============================
+                                AUTH ROUTES
+                            ============================ */}
+                            <Route path="/login" element={<SignInForm />} />
+                            <Route path="/register" element={<SignUpForm />} />
+
+                            {/* ADMIN */}
+                            <Route
+                                path="/admin"
+                                element={
+                                    <AuthGuard roles={['Admin']}>
+                                        <Admin />
+                                    </AuthGuard>
+                                }
+                            />
+
+                            {/* TEACHER */}
+                            <Route
+                                path="/teacher"
+                                element={
+                                    <AuthGuard roles={['Teacher']}>
+                                        <Admin />
+                                    </AuthGuard>
+                                }
+                            />
+
+                            {/* STUDENT (Default Home) */}
+                            <Route
+                                path="/"
+                                element={
+                                    <AuthGuard roles={['Student']}>
+                                        <HomePage />
+                                    </AuthGuard>
+                                }
+                            />
+                        </Routes>
+                    </AuthProvider>
+                </div>
+            </Router>
         </>
     );
 }
